90ae52b78e03f20d5bfd209acfb783f1bafb8417
afo32 <juanc1994@gmail.com>
2023-03-09 23:30:43 +0100

    ejercicios variables1

---
+++ js-tareas/1.variables/exercise-4.js
@@ -0,0 +1,3 @@
+const character = {name: 'Jack Sparrow', age: 10};
+var age = 25;
+console.log(age);
---
+++ js-tareas/1.variables/exercise-7.js
@@ -0,0 +1,10 @@
+let globalBasePrice = 10000;
+const car1 = {name: 'BMW m&m', basePrice: 50000, finalPrice: 60000};
+const car2 = {name: 'Chevrolet Corbina', basePrice: 70000, finalPrice: 80000};
+globalBasePrice = 25000;
+
+let arr = [car1, car2]
+for (coche of arr) {
+    coche.finalPrice = coche.basePrice + globalBasePrice;
+    console.log(coche)
+}
